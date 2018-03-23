3d862e332fa608baeeecfa7bc7ae222d6061f583
taras16 <taras.yurychko@gmail.com>
2018-03-23 21:09:15 +0200

    hte sysn

---
+++ Person/src/main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+		Person a = new Person();
+	Person z = new Person(1995, "Steve"); 
+	Person b = new Person (1993, "Kriper"); 
+	Person c = new Person (1444, "Zombie");
+	Person k = new Person (1990, "Marvel");
+	Person l = new Person (2006, "Bob");
+	
+	
+	
+	z.output();
+	b.output();
+	c.output();
+	k.output();
+	l.output();
+	
+	a.input();
+	a.output();
+	a.changeName();
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
